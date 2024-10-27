ca1c9ef0ccae7d78b2c5a3af06ec9e1e412d716a
JanaEsther <j.obendraufova@h2o-logic.cz>
2024-10-27 19:14:52 +0100

    přidání anonym. funkce+props text

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -4,36 +4,34 @@ import './style.css';
 
 export const HomePage = () => {
   const [menuOpened, setMenuOpened] = useState(false);
+  const [pageTitle, setPageTitle] = useState('Domů');
 
-  const handleClick = ()=>{
-    setMenuOpened(prevState => !prevState);
-  }
+  const handleClick = () => {
+    setMenuOpened((prevState) => !prevState);
+  };
 
- const handleSelectItem = ()=>{
-  setMenuOpened(false);
- } 
+  const handleSelectItem = (page) => {
+    setMenuOpened(false);
+    setPageTitle(page);
+  };
 
   return (
     <>
       <header>
         <div className={`menu ${menuOpened ? '' : 'menu--closed '}`}>
-          <button
-            className= "menu__btn" onClick={handleClick}
-          ></button>
+          <button className="menu__btn" onClick={handleClick}></button>
           <div className={`menu__items ${menuOpened ? '' : 'menu--closed  '}`}>
-            <MenuItem text="Domů" onSelect={handleSelectItem} />
-            <MenuItem text="Naše nabídka" onSelect={handleSelectItem} />
-            <MenuItem text="Náš tým" onSelect={handleSelectItem} />
-            <MenuItem text="Blog" onSelect={handleSelectItem} />
-            <MenuItem text="Kontakt" onSelect={handleSelectItem} />
+            <MenuItem text="Domů" onSelect={() => handleSelectItem('Domů')} />
+            <MenuItem text="Naše nabídka"onSelect={() => handleSelectItem('Naše nabídka')}/>
+            <MenuItem text="Náš tým"onSelect={() => handleSelectItem('Náš tým')}/>
+            <MenuItem text="Blog" onSelect={() => handleSelectItem('Blog')} />
+            <MenuItem text="Kontakt"onSelect={() => handleSelectItem('Kontakt')}/>
           </div>
         </div>
       </header>
       <main>
-        <h1>Domů</h1>
+        <h1>{pageTitle}</h1>
       </main>
     </>
   );
 };
-
-
